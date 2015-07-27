6c537009d0aefcefffbc010fd80ce4c511cd2753
Gian Lorenzo Meocci <glmeocci@gmail.com>
2015-07-27 20:41:48 +0200

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,1 +1,6 @@
-g++ -std=c++11 -O3 src/aggregate.cpp -o aggregate -lboost_system -lboost_filesystem
+#!/bin/bash
+
+CXX=g++4.9
+CXXFLAGS="-std=c++11 -O3"
+
+$CXX $CXXFLAGS src/aggregate.cpp -o aggregate -lboost_system -lboost_filesystem

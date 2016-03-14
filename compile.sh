a70e066b2617247fc603ff33e391d75cbfc81736
Gian Lorenzo Meocci <gianlorenzo.meocci@commprove.com>
2016-03-14 18:23:02 +0100

    changed compile script

--- compile.sh
+++ compile.sh
@@ -4,7 +4,7 @@ CXX=g++-4.9
 CC=gcc-4.9
 
 CXXFLAGS="-std=c++1y -O3"
-CCFLAGS="-std=c99 -Wall -Wextra -Wshadow -Wcast-qual -Wcast-align -Wstrict-prototypes -Wstrict-aliasing=1 -Wswitch-enum -Wundef -pedantic "
+CCFLAGS="-O3 -std=c99 -Wall -Wextra -Wshadow -Wcast-qual -Wcast-align -Wstrict-prototypes -Wstrict-aliasing=1 -Wswitch-enum -Wundef -pedantic "
 
 $CC $CCFLAGS -c xxHash/xxhash.c
 $CXX $CXXFLAGS src/aggregate.cpp xxhash.o -IxxHash/ -o aggregate -lboost_system -lboost_filesystem
